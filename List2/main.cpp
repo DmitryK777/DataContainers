f2c8b62e8564a8801f2487f84d7ce88ab9b7890e
DmitryK777 <kislyakovdmv@gmail.com>
2024-09-22 16:31:04 +0300

    List2

---
+++ List2/main.cpp
@@ -0,0 +1,107 @@
+﻿#include<iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define tab "\t"
+#define delimiter "\n-------------------------------------------"
+
+
+class List
+{
+	class Element
+	{
+		int Data;
+		Element* pNext;
+		Element* pPrev;
+
+	public:
+		Element(int Data, Element* pNext = nullptr, Element* pPrev = nullptr)
+			:Data(Data), pNext(pNext), pPrev(pPrev)
+		{
+			cout << "EConstructor:\t" << this << endl;
+
+		}
+
+		~Element()
+		{
+			cout << "EDestructor:\t" << this << endl;
+		}
+
+		friend class List;
+
+	} *Head, *Tail; // Объявляем два указателя на объекты класса Element непосредственно после описания класса
+
+	size_t size; // Количество элементов списка
+
+public:
+	// Конструктор по умолчанию создаёт пустой список
+	List()
+	{
+		Head = Tail = nullptr;
+		size = 0;
+		cout << "LConstructor" << this << endl;
+	}
+
+	~List()
+	{
+		cout << "LDestructor" << this << endl;
+	}
+
+	// Adding Elements
+	void push_front(int Data)
+	{
+		if (Head == nullptr && Tail == nullptr) Head = Tail = new Element(Data);
+		else
+		{
+			// 1) Создаём новый элемент
+			Element* New = new Element(Data);
+
+			// 2) Присоединяем элемент к списку
+			New->pNext = Head;
+
+			// 3) Присоединяем список к элементу
+			Head->pPrev = New;
+
+			// 4) Смещаем голову на новый элемент
+			Head = New;
+		}
+		size++;
+	}
+
+	// Methods
+	void print()const
+	{
+		for (Element* Temp = Head; Temp; Temp = Temp->pNext)
+			cout << Temp->pPrev << tab << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
+
+		cout << "Tail:\t" << Tail << endl;
+		cout << "Количество элементов списка: " << size << endl;
+	}
+
+	void reverse_print()const
+	{
+		for (Element* Temp = Tail; Temp; Temp = Temp->pPrev)
+			cout << Temp->pPrev << tab << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
+		cout << "Tail:\t" << Tail << endl;
+		cout << "Количество элементов списка: " << size << endl;
+	}
+};
+
+
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int n;
+	cout << "Введите размер списка: "; cin >> n;
+	
+	List list;
+	for (int i = 0; i < n; i++)
+	{
+		list.push_front(rand()%100);
+	}
+
+	list.print();
+	list.reverse_print();
+}
